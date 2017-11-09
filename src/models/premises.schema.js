03fcf3d00b7a84628fd218464fc78e5351185cfb
mewild <michael@wildapplications.com>
2017-11-09 15:29:28 +0000

    Ability to open a premises

--- src/models/premises.schema.js
+++ src/models/premises.schema.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var schema = new mongoose.Schema({
   name : { type : String, required : true },
   owner: { type : Number, required : true, index: {unique: true} },
-  description: { type : String, required: false }
+  description: { type : String, required: false },
+  open: { type: Boolean, required: true, default: false}
 }, {
   timestamps: true
 });
