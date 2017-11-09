e605a258a6738b8247c74f4382922c653375ce77
mewild <michael@wildapplications.com>
2017-11-09 15:00:06 +0000

    Ability to open a premises

--- src/helpers/premises.helper.js
+++ src/helpers/premises.helper.js
@@ -11,6 +11,13 @@ errors = require('../errors/errors.json');
 //var jwt = require('jsonwebtoken');
 //var tokenService = require('bleuapp-token-service').createTokenHandler('service.token', '50051');
 
+var grpc = require("grpc");
+var paymentDescriptor = grpc.load(__dirname + '/../proto/payment.proto').payment;
+var paymentClient = new paymentDescriptor.PaymentService('service.payment:1295', grpc.credentials.createInsecure());
+
+var menuDescriptor = grpc.load(__dirname + '/../proto/menu.proto').menu;
+var menuClient = new menuDescriptor.MenuService('service.menu:1295', grpc.credentials.createInsecure());
+
 var premises = {};
 
 premises.get = function(call, callback){
